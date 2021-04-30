f500cf84b56e942391cb80c216db6887fa8c1a8d
Sam Gold <samg915757@gmail.com>
2021-04-30 16:19:17 -0600

    nav link set up for navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar(){
-return <h1>NavBar</h1>
+  return (
+    <header>
+      <div>
+        <nav>
+          <NavLink to="/" exact>
+            SAM'S SITE
+          </NavLink>
+          <NavLink to="/post">
+            Blog Posts
+          </NavLink>
+          <NavLink to="/project">
+            Projects
+          </NavLink>
+          <NavLink to="/about">
+            About Me!
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  )
 }
