66d176a11b225df05539ceeb1e011570e07e8776
Sam Gold <samg915757@gmail.com>
2021-05-04 16:11:58 -0600

    structuring posts page... next up is mapping all posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import sanityClient from "../client.js"
 
 // 2 ways to query Sanity Studio... GROQ (created inhouse w/ Sanity), and GraphQL... WE ARE USING GROQ
@@ -25,14 +26,18 @@ export default function Post(){
   }, []);
   
   return (
-    <main>
-      <section>
-          <h1></h1>
-          <h2></h2>
-          <div>
+    <main className="bg-pink-200 min-h-screen p-12">
+      <section className="container mx-auto">
+          <h1 className="text-5xl flex justify-center">
+            BLOGS
+          </h1>
+          <h2 className="text-lg text-gray-600 flext justify-center mb-12">
+            HERE ARE ALL MY BLOG POSTS!
+          </h2>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
             <article>
-              <Link>
-                <span>
+              <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+                <span className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400" key={index}>
                   <img />
                     <span>
                       <h3></h3>
