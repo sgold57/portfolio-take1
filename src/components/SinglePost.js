01f84f911b348909495bec8be29ef7b485bbd1f2
Sam Gold <samg915757@gmail.com>
2021-05-07 20:35:01 -0600

    restyled singlePost page

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -41,7 +41,7 @@ export default function SinglePost(){
       <article className="container shadow-lg mx-auto bg-green-100 rounded-lg">
         <header className="relative">
           <div className="absolute h-full w-full flex items-center justify-center p-8">
-            <div className="bg-white bg-opacity-75 rounded p-12">
+            <div className="bg-white bg-opacity-100 rounded p-12">
               <h1 className="text-3xl lg:text-6xl mb-4">
                 {singlePost.title}
               </h1>
