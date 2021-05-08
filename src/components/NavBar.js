7f60e63f7204a11ed8b213f62514bd84bb1b143a
Sam Gold <samg915757@gmail.com>
2021-05-07 20:34:38 -0600

    restyled NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ export default function NavBar(){
             to="/" 
             exact 
             activeClassName="text-white"
-            className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold tracking-widest"
+            className="inflex-flex items-center py-8 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold tracking-widest"
           >
             SAM'S SITE
           </NavLink>
