0474bd769a3dd5bbd5ae99b34581d0149e29363c
Sam Gold <samg915757@gmail.com>
2021-05-17 21:55:18 -0600

    navbar styled for medium breakpoint

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,12 @@ import React from "react";
 
 export default function Home(){
   return (
-    <main className="bg-yellow-300">
+    <main className="bg-blue-600">
       <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-        <h1 className="text-6xl text-blue-200 font-bold">Welcome To My Home Page. I Hope That You Like It!</h1>
+        <h1 className="text-6xl text-yellow-400 font-bold">SAM GOLD</h1>
+        <h2>{`DENVER <------ NYC`}</h2>
+        <h2>SOFTWARE ENGINEER</h2>
+        <h2>PROBLEM SOLVER</h2>
       </section>
     </main>
   )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { SocialIcon } from "react-social-icons";
 
 export default function NavBar(){
   return (
-    <header className="bg-black sticky top-0 h-24">
+    <header className="bg-gray-400 sticky top-0 h-24">
       <div className="w-full flex mx-2">
         <nav className="flex">
           <NavLink 
@@ -13,14 +13,14 @@ export default function NavBar(){
             activeClassName="text-white"
             //OLD CSS className="inflex-flex px-3 mx-4 my-7 sm:my-5 sm:px-0  text-red-700 hover:text-yellow-300 text-5xl headerFont font-bold tracking-wider sm:text-lg"
             
-            className="inline-flex headerFont font-bold tracking-wider items-center text-3xl ml-7 mr-2 mt-8"
+            className="inline-flex headerFont text-indigo-800 font-bold tracking-wide md:tracking-wider items-center text-3xl md:text-4xl mx-3 mt-6 md:ml-11 md:mr-4"
           >
             SAM GOLD
           </NavLink>
           <NavLink 
             to="/post" 
             //OLD CSS className="inline-flex items-center px-4 mt-7 mb-8 sm:mx-2 rounded text-3xl headerFont text-green-600 hover:text-red-800 sm:text-sm"
-            className="inline-flex items-center text-lg text-green-600 mx-2 mt-7"
+            className="inline-flex headerFont items-center text-blue-700 text-xl md:text-2xl px-2 md:px-3 mt-7"
             
             
             activeClassName="text-yellow-300 bg-red-400 bg-opacity-40 hover:text-yellow-300"
@@ -30,18 +30,18 @@ export default function NavBar(){
           <NavLink 
             to="/project" 
             // OLD CSS className="inline-flex items-center px-4 mt-7 mb-8 sm:mx-2 rounded text-3xl headerFont text-green-600 hover:text-red-800 sm:text-sm"
-            className="inline-flex items-center text-lg text-green-600 mx-2 mt-7"
+            className="inline-flex headerFont items-center text-xl md:text-2xl font-bold text-blue-700 mx-2 md:mx-3 mt-7"
             
             
             
-            activeClassName="inline-flex items-center text-lg text-green-600 mx-2 mt-7"
+            activeClassName="inline-flex items-center text-lg text-green-600 mx-2 md:mx-3 mt-7"
           >
             Projects
           </NavLink>
           <NavLink 
             to="/about" 
             // OLD CSS className="inline-flex items-center px-4 mt-7 mb-8 sm:mx-2 sm:my-5 sm:px-0 rounded text-3xl headerFont text-green-600 hover:text-red-800 sm:text-sm"
-            className="inline-flex items-center text-lg text-green-600 mx-2 mt-7"
+            className="inline-flex headerFont items-center text-xl md:text-2xl font-bold text-blue-700 ml-3 mr-4 md:mr-1 mt-7"
             
             
             activeClassName="text-yellow-300 bg-red-400 bg-opacity-40 hover:text-yellow-300"
@@ -49,10 +49,10 @@ export default function NavBar(){
             About
           </NavLink>
         </nav>
-        <div className="inline-flex mx-3">
+        <div className="inline-flex mx-4">
           <SocialIcon 
             url="https://www.linkedin.com/in/samgold57/" 
-            className="mx-3 mt-7" 
+            className="mx-2 md:mx-4 mt-7" 
             target="_blank" //opens new tab
             fgColor="#fff" 
             style={{ height: 45, width: 45 }}
