c33b2c33e76660f7271df3079c82209cc33b8135
Sam Gold <samg915757@gmail.com>
2021-05-19 00:02:19 -0600

    body text brought in and formatting begun for small breakpoint

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,23 @@ import HomePagePhoto from "../images/denver_hp.jpeg"
 export default function Home(){
   return (
     <main className="bg-blue-700 min-h-screen">
-      <section className="flex space-between mx-12 px-8 py-20 border-2 border-yellow-400">
-        <div className=" w-1/2 border-2 border-pink-600">
-          <img src={HomePagePhoto}></img>
+      <section className="flex space-between ml-4s pt-20 pb-20 px-8">
+        <div className="w-3/4">
+          <img className="shadow-2xl" src={HomePagePhoto}></img>
         </div>
-        <div className="absolute inline-block left-1/2 max-w-full">
-          <div className="flex flex-col space-even text-3xl">
-            <div>
-              SOFTWARE ENGINEER
+        <div className="inline-block max-w-full">
+          <div className="flex flex-col bodyFont font-extrabold space-even text-5xl">
+            <div className="mt-3 ml-3 text-red-700">
+              SOFTWARE
             </div>
-            <div>
-              PROBLEM SOLVER
+            <div className="ml-7 text-yellow-300">
+              ENGINEER
+            </div>
+            <div className="mt-9 ml-4 text-red-700">
+              PROBLEM
+            </div>
+            <div className="ml-7 text-yellow-300">
+              SOLVER
             </div>
           </div>
         </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,14 +13,14 @@ export default function NavBar(){
             activeClassName="text-white"
             //OLD CSS className="inflex-flex px-3 mx-4 my-7 sm:my-5 sm:px-0  text-red-700 hover:text-yellow-300 text-5xl headerFont font-bold tracking-wider sm:text-lg"
             
-            className="inline-flex headerFont text-red-600 font-extrabold tracking-wide md:tracking-wider lg:tracking-wide items-center text-3xl lg:text-6xl xl:text-7xl ml-3 mr-6 mt-6 md:ml-11 lg:ml-8 md:mr-4"
+            className="inline-flex headerFont text-red-600 font-bold tracking-wide md:tracking-wider lg:tracking-wide items-center text-3xl lg:text-6xl xl:text-7xl ml-3 mr-6 mt-6 md:ml-11 lg:ml-8 md:mr-4"
           >
             SAM GOLD
           </NavLink>
                     <NavLink 
             to="/project" 
             // OLD CSS className="inline-flex items-center px-4 mt-7 mb-8 sm:mx-2 rounded text-3xl headerFont text-green-600 hover:text-red-800 sm:text-sm"
-            className="inline-flex tabFont items-center text-blue-700 tracking-tight text-lg md:text-2xl lg:text-3xl xl:text-4xl font-semibold mx-2 md:mx-2 mt-7 lg:mt-3 xl:ml-12"
+            className="inline-flex tabFont items-center text-blue-700 tracking-tight text-lg md:text-2xl lg:text-3xl xl:text-4xl font-semibold mx-2 md:mx-2 mt-5 lg:mt-3 xl:ml-12"
             
             
             
@@ -31,7 +31,7 @@ export default function NavBar(){
           <NavLink 
             to="/post" 
             //OLD CSS className="inline-flex items-center px-4 mt-7 mb-8 sm:mx-2 rounded text-3xl headerFont text-green-600 hover:text-red-800 sm:text-sm"
-            className="inline-flex tabFont items-center text-blue-700 tracking-tight text-lg md:text-2xl lg:text-3xl xl:text-4xl font-semibold tracking:wide mx-2 md:mx-2 mt-7 lg:mt-3 xl:ml-12"
+            className="inline-flex tabFont items-center text-blue-700 tracking-tight text-lg md:text-2xl lg:text-3xl xl:text-4xl font-semibold tracking:wide mx-2 md:mx-2 mt-5 lg:mt-3 xl:ml-12"
             
             
             activeClassName="text-yellow-300 bg-red-400 bg-opacity-40 hover:text-yellow-300"
@@ -41,7 +41,7 @@ export default function NavBar(){
           <NavLink 
             to="/about" 
             // OLD CSS className="inline-flex items-center px-4 mt-7 mb-8 sm:mx-2 sm:my-5 sm:px-0 rounded text-3xl headerFont text-green-600 hover:text-red-800 sm:text-sm"
-            className="inline-flex tabFont items-center text-lg md:text-2xl lg:text-3xl xl:text-4xl font-semibold text-blue-700 tracking-tight mx-2 md:mr-1 xl:mr-5 mt-7 lg:mt-3 xl:ml-12"
+            className="inline-flex tabFont items-center text-lg md:text-2xl lg:text-3xl xl:text-4xl font-semibold text-blue-700 tracking-tight mx-2 md:mr-1 xl:mr-5 mt-5 lg:mt-3 xl:ml-12"
             
             
             activeClassName="text-yellow-300 bg-red-400 bg-opacity-40 hover:text-yellow-300"
@@ -52,17 +52,17 @@ export default function NavBar(){
         <div className="inline-flex mx-4">
           <SocialIcon 
             url="https://www.linkedin.com/in/samgold57/" 
-            className="mx-4 mt-7" 
+            className="mx-4 mt-5" 
             target="_blank" //opens new tab
             fgColor="#fff" 
-            style={{ height: 40, width: 40 }}
+            style={{ height: 45, width: 45 }}
           />
           <SocialIcon 
             url="https://github.com/sgold57" 
-            className="mr-4 mt-7" 
+            className="mr-4 mt-5" 
             target="_blank" //opens new tab
             fgColor="#fff" 
-            style={{ height: 40, width: 40 }}
+            style={{ height: 45, width: 45 }}
           />
         </div>
       </div>
