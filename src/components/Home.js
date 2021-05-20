6bc7999aaac5912c12caf8ace89027cd6bac8056
Sam Gold <samg915757@gmail.com>
2021-05-19 21:18:59 -0600

    homepage flexbox issues sorted

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,21 +5,21 @@ export default function Home(){
   return (
     <main className="bg-blue-700 min-h-screen">
       <section className="flex space-between ml-4s pt-20 pb-20 px-8">
-        <div className="w-3/4">
+        <div className="w-1/2">
           <img className="shadow-2xl" src={HomePagePhoto}></img>
         </div>
-        <div className="inline-block max-w-full">
+        <div className="inline-block w-1/2 max-w-full border-2 border-yellow-200">
           <div className="flex flex-col bodyFont font-extrabold space-even text-5xl">
-            <div className="mt-3 ml-3 text-red-700">
+            <div className="mt-3 ml-3 text-red-700 text-center">
               SOFTWARE
             </div>
-            <div className="ml-7 text-yellow-300">
+            <div className="ml-3 text-center text-yellow-300">
               ENGINEER
             </div>
-            <div className="mt-9 ml-4 text-red-700">
+            <div className="mt-9 ml-4 text-center text-red-700">
               PROBLEM
             </div>
-            <div className="ml-7 text-yellow-300">
+            <div className="ml-4 text-center text-yellow-300">
               SOLVER
             </div>
           </div>
