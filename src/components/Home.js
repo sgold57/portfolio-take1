9d27c3728c130462b29b292dda6a302437c5ca63
Sam Gold <samg915757@gmail.com>
2021-05-19 22:19:16 -0600

    homepage styled for all breakpoints

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,20 +6,20 @@ export default function Home(){
     <main className="bg-blue-700 min-h-screen">
       <section className="flex space-between ml-4s pt-20 pb-20 px-8">
         <div className="w-1/2">
-          <img className="shadow-2xl" src={HomePagePhoto}></img>
+          <img src={HomePagePhoto}></img>
         </div>
-        <div className="inline-block w-1/2 max-w-full border-2 border-yellow-200">
-          <div className="flex flex-col bodyFont font-extrabold space-even text-5xl">
-            <div className="mt-3 ml-3 text-red-700 text-center">
+        <div className="inline-block w-1/2 max-w-full">
+          <div className="flex flex-col bodyFont font-extrabold space-even text-5xl md:text-6xl lg:text-8xl">
+            <div className="mt-3 lg:mt-9 ml-3 text-yellow-300 text-center">
               SOFTWARE
             </div>
-            <div className="ml-3 text-center text-yellow-300">
+            <div className="ml-3 text-center text-red-700">
               ENGINEER
             </div>
-            <div className="mt-9 ml-4 text-center text-red-700">
+            <div className="mt-9 lg:mt-12 ml-2 text-center text-yellow-300">
               PROBLEM
             </div>
-            <div className="ml-4 text-center text-yellow-300">
+            <div className="ml-3 text-center text-red-700">
               SOLVER
             </div>
           </div>
