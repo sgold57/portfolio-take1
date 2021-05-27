016fc398cae095a359bf774482bbf0511a760d9a
Sam Gold <samg915757@gmail.com>
2021-05-26 21:13:54 -0600

    changed homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,19 +9,20 @@ export default function Home(){
           <img className="shadow-xl" src={HomePagePhoto}></img>
         </div>
         <div className="inline-block w-1/2 max-w-full">
-          <div className="flex flex-col bodyFont font-extrabold space-even text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
-            <div className="mt-3 lg:mt-9 ml-3 text-yellow-200 text-center">
+          <div className="flex flex-col bodyFontfont-extrabold space-even text-left  text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
+            <div className="mt-3 lg:mt-9 ml-3 text-yellow-200 text-left">
               SOFTWARE
             </div>
-            <div className="ml-3 text-center text-red-500">
+            <div className="ml-3 text-left text-red-500">
               ENGINEER
             </div>
-            <div className="mt-9 lg:mt-12 ml-2 text-center text-yellow-200">
-              PROBLEM
+            <div className="mt-9 lg:mt-12 ml-2 text-left text-yellow-200">
+              90'S RAP
             </div>
-            <div className="ml-3 text-center text-red-500">
-              SOLVER
+            <div className="ml-3 text-left text-red-500">
+              CONNOISSEUR
             </div>
+
           </div>
         </div>
       </section>
