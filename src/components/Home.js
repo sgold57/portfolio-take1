4f1c663fa54e99e4e2182ec0850648cf86644bd9
Sam Gold <samg915757@gmail.com>
2021-06-02 22:42:08 -0600

    styling done to homepage to get text

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,13 @@ import HomePagePhoto from "../images/denver_hp_with_lines_bigger.png"
 export default function Home(){
   return (
     <main className="flex bg-blue-700 min-h-screen min-w-screen">
-      <section className="flex w-screen justify-start space-between ml-4 mt-32 border-8 border-red-600">
+      <section className="flex max-w-screen justify-start space-between ml-4 mt-32 border-8 border-red-600">
         <div className="border-solid border-5 border-pink-300 self-center">
           <img className="w-full" src={HomePagePhoto}></img>
         </div>
-        {/* <div className="inline-block w-1/2 max-w-full border-solid border-8 border-yellow-600">
-          <div className="flex absolute flex-col bodyFontfont-extrabold space-even text-left  text-5xl md:text-6xl lg:text-7xl xl:text-8xl border-solid border-4 border-yellow-500">
+        <div className="inline my-48 px-24 border-solid border-8 border-yellow-600">
+        </div> 
+          {/* <div className="flex absolute flex-col bodyFontfont-extrabold space-even text-left  text-5xl md:text-6xl lg:text-7xl xl:text-8xl border-solid border-4 border-yellow-500">
             <div className="mt-2 lg:mt-9 ml-1 text-yellow-200 text-left">
               SOFTWARE
             </div>
@@ -23,9 +24,11 @@ export default function Home(){
               CONNOISSEUR
             </div>
 
-          </div>
-        </div> */}
+          </div> */} 
       </section>
+      <div className= "-mx-96 mt-52 mb-80 px-8 border-solid border-8 border-yellow-600">
+      
+      </div>
     </main>
   )
 
