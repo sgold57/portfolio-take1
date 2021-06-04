07b2fac0b58704ae942be802e860fb3b66b82c5b
Sam Gold <samg915757@gmail.com>
2021-06-03 23:53:52 -0600

    small changes made...more tmrw

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,19 +7,19 @@ export default function Home(){
       <section className="relative flex max-w-screen top-12 py-24 border-8 border-red-600">
         <img className="border-solid border-5 border-green-300 self-center w-full" src={HomePagePhoto}></img>
         <div className="absolute flex-col content-evenly inset-y-0 left-1/2 w-1/2 homeLabelChildPadding border-8 border-solid border-red-400 text-lg lg:text-5xl">
-          <div className="border-solid border-2 mt-16 p-8 border-gray-600 bodyFont font-extrabold ">
+          <div className="border-solid border-2 mt-2 lg:mt-16 py-8 px-6 border-gray-600 bodyFont font-extrabold ">
             <span className="text-yellow-200">SOFTWARE</span>
-            <span className="text-red-500 m-6">ENGINEER</span>
+            <span className="text-red-500 m-2 lg:m-6">ENGINEER</span>
           </div>
-          <div className="border-solid border-2 mt-16 p-8 border-yellow-600 bodyFont font-extrabold text-yellow-200 text-lg lg:text-5xl">
+          <div className="border-solid border-2 lg:mt-16 p-8 border-yellow-600 bodyFont font-extrabold text-yellow-200 text-lg lg:text-5xl">
             <span className="text-red-500">PROBLEM</span>
-            <span className="text-yellow-200 m-6">SOLVER</span>
+            <span className="text-yellow-200 m-4 lg:m-6">SOLVER</span>
           </div> 
-          <div className="border-solid border-2 mt-28 p-8 border-yellow-600 bodyFont font-extrabold text-yellow-200 text-lg lg:text-5xl">
+          <div className="border-solid border-2 lg:mt-28 p-8 border-yellow-600 bodyFont font-extrabold text-yellow-200 text-lg lg:text-5xl">
             <span className="text-yellow-200 pr-6 text-lg lg:text-4xl border-solid border-2">90'S RAP</span>
             <span className="text-red-500 ml-9">CONNOISSEUR</span> 
           </div> 
-          <div className="border-solid border-2 mt-16 p-8 border-yellow-600 bodyFont font-extrabold text-yellow-200 lg:text-5xl">
+          <div className="border-solid border-2 lg:mt-16 p-8 border-yellow-600 bodyFont font-extrabold text-yellow-200 lg:text-5xl">
             <span className="text-yellow-200">BASKETBALL</span>
             <span className="text-red-500 m-6">JUNKIE</span> 
           </div> 
