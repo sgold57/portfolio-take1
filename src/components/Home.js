4f19d1a10776e1575917df26389486422dfc2a9b
Sam Gold <samg915757@gmail.com>
2021-06-04 18:47:42 -0600

    characteristics styled for small breakpoint

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,24 +4,24 @@ import HomePagePhoto from "../images/denver_hp_with_lines_bigger.png"
 export default function Home(){
   return (
     <main className="relative bg-blue-700 min-h-screen w-screen mt-20 lg:mt-24 xl:mt-28">
-      <section className="relative flex max-w-screen top-12 py-24 border-8 border-red-600">
-        <img className="border-solid border-5 border-green-300 self-center w-full" src={HomePagePhoto}></img>
-        <div className="absolute flex-col content-evenly inset-y-0 left-1/2 w-1/2 homeLabelChildPadding border-8 border-solid border-red-400 text-lg lg:text-5xl">
-          <div className="border-solid border-2 mt-2 lg:mt-16 py-8 px-6 border-gray-600 bodyFont font-extrabold ">
+      <section className="relative flex max-w-screen top-12 py-24">
+        <img className=" self-center w-full" src={HomePagePhoto}></img>
+        <div className="absolute flex-col content-evenly inset-y-0 left-1/2 w-1/2 homeLabelChildPadding text-lg sm:text-2xl lg:text-5xl">
+          <div className="mt-2 sm:mt-10 lg:mt-16 pt-12 pb-4 px-5 sm:pl-5 border-gray-600 bodyFont font-extrabold ">
             <span className="text-yellow-200">SOFTWARE</span>
             <span className="text-red-500 m-2 lg:m-6">ENGINEER</span>
           </div>
-          <div className="border-solid border-2 lg:mt-16 p-8 border-yellow-600 bodyFont font-extrabold text-yellow-200 text-lg lg:text-5xl">
+          <div className="lg:mt-16 py-7 px-5 sm:pl-5 border-yellow-600 bodyFont font-extrabold">
             <span className="text-red-500">PROBLEM</span>
-            <span className="text-yellow-200 m-4 lg:m-6">SOLVER</span>
+            <span className="text-yellow-200 m-3 lg:m-6">SOLVER</span>
           </div> 
-          <div className="border-solid border-2 lg:mt-28 p-8 border-yellow-600 bodyFont font-extrabold text-yellow-200 text-lg lg:text-5xl">
-            <span className="text-yellow-200 pr-6 text-lg lg:text-4xl border-solid border-2">90'S RAP</span>
-            <span className="text-red-500 ml-9">CONNOISSEUR</span> 
+          <div className="lg:mt-28 pt-16 pb-7 sm:pl-5 bodyFont font-extrabold">
+            <span className="text-yellow-200 lg:text-4xl">90'S RAP</span>
+            <span className="text-red-500 sm:ml-2">ENTHUSIAST</span> 
           </div> 
-          <div className="border-solid border-2 lg:mt-16 p-8 border-yellow-600 bodyFont font-extrabold text-yellow-200 lg:text-5xl">
-            <span className="text-yellow-200">BASKETBALL</span>
-            <span className="text-red-500 m-6">JUNKIE</span> 
+          <div className="lg:mt-16 pt-6 sm:pl-5 bodyFont font-extrabold border-solid border-2">
+            <span className="text-red-500">BASKETBALL</span>
+            <span className="text-yellow-200 pl-2 lg:m-6">JUNKIE</span> 
           </div> 
           
         </div>
