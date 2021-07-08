b76a94c82f777b085933a9adece72e19677fc223
Sam Gold <samg915757@gmail.com>
2021-07-08 13:34:05 -0600

    spotify playlist link added

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ export default function Home({ getBballLink, bballLinks }){
     // mt-20 lg:mt-24 xl:mt-28
     <main className="bg-blue-700 bg-opacity-90 h-screen w-screen overflow-auto">
       <section className="relative flex max-w-screen top-12 lg:top-8 py-8">
-        <img className="self-center max-w-screen opacity-90" src={HomePagePhoto}></img>
+        <img className="self-center max-w-screen opacity-90" src={HomePagePhoto} alt="Denver Graphic"></img>
         <div className="absolute textDiv flex-col content-evenly inset-y-8 lg:inset-y-1/2 lg:mt-10 left-1/2 w-1/2 text-2xl md:text-4xl lg:text-4xl xl:text-5xl">
           <div className="flex-col lg:mr-5">
             <div className="flex justify-center md:justify-start lg:justify-end md:tracking-tighter lg:tracking-normal bodyFont font-extrabold mt-12 lg:mt-5 xl:mt-4">
@@ -23,13 +23,14 @@ export default function Home({ getBballLink, bballLinks }){
           </div>
           <div className="flex-col mt-12 sm:mt-16 md:mt-28 lg:mt-8 lg:mr-5 xl:mt-10 bodyFont font-extrabold">
             <div className="flex justify-center md:justify-start lg:justify-end md:tracking-tighter lg:tracking-normal bodyFont font-extrabold">
-              
+              <a href="https://open.spotify.com/playlist/6J6kPoch1Rg1ptd1IkxKD4?si=d958ea527d5a4130" target="_blank">
                 <span className="imageRed text-opacity-80">90'S RAP</span>
                 <span className="text-yellow-200 ml-3">ENTHUSIAST</span> 
+              </a>
             </div> 
             
             <div className="flex justify-center md:justify-start lg:justify-end md:tracking-tighter lg:tracking-normal bodyFont font-extrabold mt-2 md:mt-6 lg:mt-4">
-              <a href={getBballLink(bballLinks)} target="_blank">
+              <a href={getBballLink(bballLinks)}>
                 <span className="text-yellow-200">BASKETBALL</span>
                 <span className="imageRed text-opacity-80 ml-3">JUNKIE</span> 
               </a>
