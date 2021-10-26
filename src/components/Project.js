7c41ab4d892aabc2e68b03efd87ac0364864bc6e
Sam Gold <samg915757@gmail.com>
2021-10-25 20:03:25 -0600

    more styling done with new div

--- src/components/Project.js
+++ src/components/Project.js
@@ -49,8 +49,8 @@ export default function Project(){
                 ><u>{project.name}</u>
               </a>
             </h3>
-            <div className="attributesDiv flex flex-col sm:block justify-evenly text-md space-x-0.5 sm:space-x-4">  {/* where all the information (title, type, date) will live */}
-              <div className="flex sm:content-evenly lg:content-center py-1 sm:pl-28 md:pl-36 lg:pl-64 xl:pl-40 text-sm sm:text-lg lg:text-xl">
+            <div className="attributesDiv flex flex-col sm:block justify-evenly gap-y-2 text-md space-x-0.5 sm:space-x-4">  {/* where all the information (title, type, date) will live */}
+              <div className="flex sm:content-evenly py-1 sm:pl-28 md:pl-48 lg:pl-64 xl:pl-32 text-sm sm:text-lg lg:text-xl">
                 <span className="text-red-500 sm:mr-2 sm:mt-4">
                   <strong className="techSpan font-bold text-blue-700 mr-2">Technologies Used:</strong>{"  "}
                 </span>
@@ -58,11 +58,11 @@ export default function Project(){
                   <img className="techIcon w-10 h-10 pr-1 sm:w-11 sm:h-11 lg:h-15 lg:w-15 sm:pr-2 sm:mb-3 sm:mt-1" src={techUrl} alt="UNKNOWN" />
                   )}                  
               </div>
-              <span className="fiSpan text-red-500 text-base lg:text-xl sm:pl-20 md:pl-44 lg:pl-64 xl:pl-40 py-2">
+              <span className="fiSpan text-red-500 text-base lg:text-xl sm:pl-20 md:pl-48 lg:pl-64 xl:pl-32 py-2">
                 <strong className="sm:font-bold text-blue-700 tracking-normal text-center">Institution:</strong>{"  "}
                 {project.institution}
               </span>
-              <div className="flex justify-center gap-x-9">
+              <div className="flex justify-center gap-x-3 md:gap-x-9">
               
                 <span className="apiSpan text-base lg:text-xl text-red-500 py-2">
                   <a 
